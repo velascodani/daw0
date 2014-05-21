64d7551e0b7a9645c567d1e39dcce8a82ce8b2e2
Olga López <>
2014-05-21 13:51:02 +0200

    Practica-2. Creamos clase y constructor

---
+++ src/java/beans/Producto.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+import java.util.Date;
+
+/**
+ *
+ * @author OCAD
+ * 21/05/14- Creamos clase y constructor
+ */
+public class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+    private String descripcion;
+    private Date ultimaActualizacion;
+    private String imagen;
+    private int categoriaid;
+
+    public Producto(int id, String nombre, double precio, String descripcion, Date ultimaActualizacion, String imagen, int categoriaid) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.ultimaActualizacion = ultimaActualizacion;
+        this.imagen = imagen;
+        this.categoriaid = categoriaid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getUltimaActualizacion() {
+        return ultimaActualizacion;
+    }
+
+    public void setUltimaActualizacion(Date ultimaActualizacion) {
+        this.ultimaActualizacion = ultimaActualizacion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public int getCategoriaid() {
+        return categoriaid;
+    }
+
+    public void setCategoriaid(int categoriaid) {
+        this.categoriaid = categoriaid;
+    }
+
+   
+    
+}
