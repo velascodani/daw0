9749d7a57de3b892127f3bdb330726df10f9967f
Daniel Velasco <>
2014-06-18 12:03:14 +0200

    Creado la classe CLIENTE (constructores, getter & setter, to string...)

--- src/java/beans/Cliente.java
+++ src/java/beans/Cliente.java
@@ -11,5 +11,76 @@ package beans;
  * @author FO-Mañana
  */
 public class Cliente {
+    int id;
+    String nombre;
+    String email;
+    String telefono;
+    String direcion;
+    String tarjeta;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id, String nombre, String email, String telefono, String direcion, String tarjeta) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.direcion = direcion;
+        this.tarjeta = tarjeta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDirecion() {
+        return direcion;
+    }
+
+    public void setDirecion(String direcion) {
+        this.direcion = direcion;
+    }
+
+    public String getTarjeta() {
+        return tarjeta;
+    }
+
+    public void setTarjeta(String tarjeta) {
+        this.tarjeta = tarjeta;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", email=" + email + ", telefono=" + telefono + ", direcion=" + direcion + ", tarjeta=" + tarjeta + '}';
+    }
     
 }
